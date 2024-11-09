97b799e589d2b5c8153c67f740587d48fa9998c8
Jason Joannou <jjoannou@bscglobal.com>
2024-11-09 15:47:20 +0200

    Almost done q1

--- stokvel_algorand.py
+++ stokvel_algorand.py
@@ -1,4 +1,5 @@
 import argparse
+import random
 import time
 import webbrowser
 from typing import Any, Dict, List, Optional, Tuple
@@ -100,38 +101,41 @@ class SingleSigTransaction:
         sender: Account,
         receiver: Account,
         amount: int,
-        multi_sig_accoutns: List[Account],
     ) -> None:
         self.sender = sender
         self.receiver = receiver
         self.amount = amount
 
     def pay(self, note: str):
-        if self.sender.check_balance() < self.amount:
-            raise InsufficientFundsError(
-                f"Insufficient funds for {self.sender.address}"
+        try:
+
+            if self.sender.check_balance() < self.amount:
+                raise InsufficientFundsError(
+                    f"Insufficient funds for {self.sender.address}"
+                )
+
+            converted_ammount = int(self.amount / self.sender.algo_conversion)
+            unsigned_txn = transaction.PaymentTxn(
+                sender=self.sender.address,
+                sp=self.sender.algod_client.suggested_params(),
+                receiver=self.receiver.address,
+                amt=converted_ammount,  # Amount variable is measured in MicroAlgos. i.e. 1 ALGO = 1,000,000 MicroAlgos
+                note=note.encode("utf-8"),
             )
 
-        converted_ammount = int(self.amount / self.sender.algo_conversion)
-        unsigned_txn = transaction.PaymentTxn(
-            sender=self.sender.address,
-            sp=self.sender.algod_client.suggested_params(),
-            receiver=self.receiver.address,
-            amt=converted_ammount,  # Amount variable is measured in MicroAlgos. i.e. 1 ALGO = 1,000,000 MicroAlgos
-            note=note.encode("utf-8"),
-        )
+            signed_txn = unsigned_txn.sign(self.sender.private_key)
 
-        signed_txn = unsigned_txn.sign(self.sender.private_key)
+            txid = self.sender.algod_client.send_transaction(signed_txn)
 
-        txid = self.sender.algod_client.send_transaction(signed_txn)
+            _ = transaction.wait_for_confirmation(self.sender.algod_client, txid)
 
-        _ = transaction.wait_for_confirmation(self.sender.algod_client, txid)
-
-        print(f"Successfully submitted transaction with txID: {txid}")
-        print(f"Sender: {self.sender.address}")
-        print(f"Receiver: {self.receiver.address}")
-        print(f"Amount: {self.amount} Algoes")
-        print(f"Note: {note}")
+            print(f"Successfully submitted transaction with txID: {txid}")
+            print(f"Sender: {self.sender.address}")
+            print(f"Receiver: {self.receiver.address}")
+            print(f"Amount: {converted_ammount} Algoes")
+            print(f"Note: {note}")
+        except Exception as e:
+            print(f"Error in SingleSig: {e}")
 
 
 class MultiSigTransaction:
@@ -154,6 +158,10 @@ class MultiSigTransaction:
     def pay(self, note: str):
 
         try:
+            if self.sender.check_balance() < self.amount:
+                raise InsufficientFundsError(
+                    f"Insufficient funds for {self.sender.address}"
+                )
             converted_amount = int(self.amount / Account.algo_conversion)
             multi_sig_addresses = [
                 participant.address for participant in self.participants
@@ -184,11 +192,11 @@ class MultiSigTransaction:
             print(f"Successfully submitted transaction with txID: {txid}")
             print(f"Sender: {self.sender.address}")
             print(f"Receiver: {self.receiver.address}")
-            print(f"Amount: {self.amount} Algoes")
+            print(f"Amount: {converted_amount} Algoes")
             print(f"Note: {note}")
 
         except Exception as e:
-            print(f"Error: {e}")
+            print(f"Error in MultiSig: {e}")
 
 
 # Utility functions
@@ -309,12 +317,7 @@ def test_transactions():
     amount = 0.5
     sum_ammount = 0
     for account in accounts:
-        SingleSigTransaction(
-            sender=account,
-            receiver=multisig_account,
-            amount=0.5,
-            multi_sig_accoutns=accounts,
-        ).pay(
+        SingleSigTransaction(sender=account, receiver=multisig_account, amount=0.5).pay(
             note=f"Payment made from participant {account.address} to stokvel address {multisig_account.address}"
         )
         sum_ammount += amount
@@ -330,5 +333,101 @@ def test_transactions():
     )
 
 
+def run_payment_simulation(
+    time_t: int, accounts: List[Account], multisig_account: Account, amount: int
+) -> None:
+    successful_payments = set()
+    i = 1
+    sum_ammount = 0
+    count_months = 1
+    while i < 31:
+        print(f"This is day {i} of month {count_months}.")
+        if i == time_t:
+            print(f"Day {i} of month {count_months} is contribution day.")
+            for account in accounts:
+                SingleSigTransaction(
+                    sender=account, receiver=multisig_account, amount=0.5
+                ).pay(
+                    note=f"Payment made from participant {account.address} to stokvel address {multisig_account.address}"
+                )
+                sum_ammount += amount
+
+        if i == time_t + 1:
+            print(f"Day {i} of month {count_months} is payout day.")
+            signature_ammounts = 0
+            payout_account = random.choice(accounts)
+            if payout_account.address not in successful_payments:
+                for account in accounts:
+                    if (
+                        input(
+                            f"Account {account.address}, signature is needed to authorize the payout. Do you want to sign the transaction? (y/n)"
+                        )
+                        == "y"
+                    ):
+                        signature_ammounts += 1
+
+                if signature_ammounts >= round(0.8 * len(accounts)):
+                    MultiSigTransaction(
+                        multisig_account=multisig_account,
+                        receiver=payout_account.address,
+                        multisig_participants=accounts,
+                        amount=sum_ammount * 0.6,
+                        threshold=round(0.8 * len(accounts)),
+                    ).pay(
+                        note=f"Payment made from stokvel address {multisig_account.address} to participant {payout_account.address}"
+                    )
+                    successful_payments.add(payout_account.address)
+                    sum_ammount = sum_ammount - sum_ammount * 0.6
+
+        if len(successful_payments) == 5:
+            for account in accounts:
+                if input("Do you want to continue? (y/n)") == "n":
+                    break
+        if i == 30:
+            i = 0
+            count_months += 1
+        if count_months == 13:
+            count_months = 1
+
+        i += 1
+
+
 if __name__ == "__main__":
-    test_transactions()
+    # test_transactions()
+    # accounts = generate_account()
+    accounts = [
+        {
+            "address": "XIYVUEEH6BAUJPZDRMWIEINY32N7XWVSBABTD2NZALUKW6UR3BEBRZ4LPA",
+            "mnemonic_phrase": "usage toy repeat muscle neck matter gloom minute quantum bracket will unfair alley target effort book oval rib portion boy margin west source abandon dirt",
+            "private_key": "fJ95bRnZSSVuTI16tUb20U6Dd9eIGe08rlCr8cPlAxq6MVoQh/BBRL8jiyyCIbjem/vasggDMem5AuirepHYSA==",
+        },
+        {
+            "address": "ZJLDM4WFX2RBZM5DCWD4FZ364H3MW6X2ELNZI3BVTXDB5FMO4QYYEF5LYY",
+            "mnemonic_phrase": "wood video relax shop palm repeat bomb lawsuit example exit health assume umbrella you near dice delay odor chalk drastic plastic now exchange about fetch",
+            "private_key": "6fc8amusz9omI35z+lPU3hD2/HNyPc9J5kskBNNc1mnKVjZyxb6iHLOjFYfC537h9st6+iLblGw1ncYelY7kMQ==",
+        },
+        {
+            "address": "THSRW54PN3QB5HWZOTEKHSTTITDI4FQXP5EHKOVCXCEKBPHDDP4CTKERDY",
+            "mnemonic_phrase": "bachelor boring congress beauty speed plastic brisk ranch uphold miss remember infant salute hungry impose enforce shadow drum chapter switch cloth regular lawn about frozen",
+            "private_key": "iXgGXjqBaJiKg7F2d2NrNWdfvT0uSibm0EzC7RXTwm+Z5Rt3j27gHp7ZdMijynNExo4WF39IdTqiuIigvOMb+A==",
+        },
+        {
+            "address": "2HZHZSFC7MUNLCXF2OAJRCJQGRXZEXGYV5FIBZOM277WYYP5JVZBE43WXQ",
+            "mnemonic_phrase": "major waste this fox tissue first adult indoor vacant wrong fluid vague install sure toss diagram summer demise fun fix fiscal auto pet absorb wink",
+            "private_key": "M+y9wcVl8V15AHODr3+zCr86aLf8PMmWDryAxSs+bNTR8nzIovso1Yrl04CYiTA0b5Jc2K9KgOXM1/9sYf1Ncg==",
+        },
+        {
+            "address": "UFM4KJQYQKBDHADZKPOW477IZMD7R5P2556EVDERUV4E6EQAE27C42T6WU",
+            "mnemonic_phrase": "tonight bird salon process chicken beach denial dragon legal risk arena track huge room syrup spoon lucky adapt wash install rent slight north abandon bean",
+            "private_key": "JKcFfbnak01MB0L7m+4WaF6375ab0iXEwO5XJ9suyxKhWcUmGIKCM4B5U91uf+jLB/j1+u98SoyRpXhPEgAmvg==",
+        },
+    ]
+    accounts = [Account(**account) for account in accounts]
+    threshold = round(0.8 * len(accounts))
+
+    multisig_account = produce_multisig_stokvel_account(
+        threshold=threshold, accounts=accounts, version=1
+    )
+    run_payment_simulation(
+        time_t=10, accounts=accounts, multisig_account=multisig_account, amount=0.5
+    )
